bd618bec739ca56974cd25593928fcc90e320517
赵一航 <2901058896@qq.com>
2026-04-20 15:55:17 +0800

    基础的场景，物体，光源渲染

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'  // 需要引入 path 模块
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')  // 假设你的源码目录是 src
+    }
+  }
 })
